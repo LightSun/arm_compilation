89de5cce6ad06da2d0916e01ba5bbb0ef63743c0
heaven7 <18111646505@163.com>
2020-09-29 15:57:41 +0800

    test add_float_neon2 ok

--- ArmCompilation/app/src/main/cpp/arm_test.c
+++ ArmCompilation/app/src/main/cpp/arm_test.c
@@ -12,7 +12,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
+/*
  * 测试内联汇编，分别根据AArch32架构以及AArch64架构来实现一个简单的减法计算
  * @param a 被减数
  * @param b 减数
@@ -35,9 +35,14 @@
 #endif
 }*/
 
-
+extern void add_float_neon2(float* dst, float* src1, float* src2, int count);
 extern int MyASMTest(int a, int b);
 
 JNICALL jint Java_com_heaven7_android_compilation_ArmCompilation_testSub(JNIEnv *env, jclass clazz, jint a , jint b){
-    return MyASMTest(a, b);
+    //return MyASMTest(a, b);
+    float result[3];
+    float b1[3] = {4, 5, 6};
+    float c1[3] = {7, 8, 9};
+    add_float_neon2(result, b1, c1, 3);
+    return 0;
 }
--- ArmCompilation/app/src/main/java/com/heaven7/android/compilation/MainActivity.java
+++ ArmCompilation/app/src/main/java/com/heaven7/android/compilation/MainActivity.java
@@ -3,12 +3,21 @@ package com.heaven7.android.compilation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("arm_study");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickTest1(View view) {
+        ArmCompilation.testSub(1, 2);
     }
 }
